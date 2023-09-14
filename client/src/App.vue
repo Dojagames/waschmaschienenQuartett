70198ee1a5dc164564c4fa8d5a62dc6dca36ac34
Jonx <dev.jonx.eu@gmail.com>
2023-09-14 08:05:59 +0200

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+
+    data(){
+        return {
+            
+        }
+    },
+    components: {
+        
+    },
+    props: {
+        
+    },
+    methods: {
+        
+    },
+    created() {
+        
+    },
+    mounted(){
+        
+    },
+    watch: {
+        
+    },
+
+}
+</script>
+
+<template>
+    test
+</template>
+
+<style scoped>
+    
+</style>
